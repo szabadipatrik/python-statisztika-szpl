2bc4ac9869c2e1dcb326937f0555427f8a341e7e
Szabadi_Patrik <X66V18@hallgato.uniduna.hu>
2025-11-28 10:38:13 +0100

    Első feltöltés próba

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import scrolledtext
-from statisztika_SZPL import szavak_szama_SZPL, karakter_szam_SZPL, atlag_szohossz_SZPL, betu_statisztika_SZPL
+from statisztika_SZPL import szavak_szama_SZPL, karakter_szam_SZPL, atlag_szohossz_SZPL, betu_statisztika_SZPL, \
+    szamjegyek_szama_SZPL
 
 
 class StatisztikaAblak_SZPL:
@@ -12,9 +13,9 @@ class StatisztikaAblak_SZPL:
         self.szoveg_bevitel.pack(pady=10, padx=10)
 
         self.keres_label = tk.Label(master,
-                                    text="Kérem írjon be egy tetszőleges szöveget a betű és szó statisztika számításához!",
+                                    text="Kérem írjon be egy tetszőleges szöveget, hogy a betűk, szavak, számok statisztikáját számolhassuk!",
                                     fg="darkblue",
-                                    font=("Helvetica", 10, "italic"))
+                                    font=("Helvetica", 12, "italic"))
         self.keres_label.pack(pady=(0, 5))
 
         self.szamitas_gomb = tk.Button(master, text="Statisztika Számítása", command=self.szamitas_vegzes_SZPL)
@@ -44,17 +45,19 @@ class StatisztikaAblak_SZPL:
         karakter_szam_szokozzel = karakter_szam_SZPL(szoveg, True)
         karakter_szam_szokoz_nelkul = karakter_szam_SZPL(szoveg, False)
 
+
         magan_szam, massal_szam = betu_statisztika_SZPL(szoveg)
+        szamjegy_szam = szamjegyek_szama_SZPL(szoveg)
 
         eredmenyek = (
             "Eredmények:\n"
             f"  Szavak száma: {szavak_szama}\n"
             f"  Karakterek száma (összesen): {karakter_szam_szokozzel}\n"
             f"  Karakterek száma (szóközök nélkül): {karakter_szam_szokoz_nelkul}\n"
-            
-            "--- Betű Statisztika ---\n"
+             "--- Betű Statisztika ---\n"
             f"  Magánhangzók száma: {magan_szam}\n"
-            f"  Mássalhangzók száma: {massal_szam}"
+            f"  Mássalhangzók száma: {massal_szam}\n"
+            f"  Számjegyek száma: {szamjegy_szam}"
         )
 
         self.szamitas_gomb.pack_forget()
--- statisztika_SZPL.py
+++ statisztika_SZPL.py
@@ -3,33 +3,48 @@ def szavak_szama_SZPL(szoveg):
     return len(szavak)
 
 
-def karakter_szam_SZPL(szoveg, szokozt_szamol=True):
-    if szokozt_szamol:
+def karakter_szam_SZPL(szoveg, szokozzel):
+    if szokozzel:
         return len(szoveg)
     else:
-        szoveg_szokoz_nelkul = "".join(szoveg.split())
-        return len(szoveg_szokoz_nelkul)
+        karakterek = [k for k in szoveg if not k.isspace()]
+        return len(karakterek)
 
 
 def atlag_szohossz_SZPL(szoveg):
     szavak = szoveg.split()
     if not szavak:
-        return 0
-    osszes_hossz = sum(len(szo) for szo in szavak)
-    return osszes_hossz / len(szavak)
+        return 0.0
+
+    # Kiszűrjük a betűket nem tartalmazó 'szavakat' (pl. csak írásjelek, vagy számok) a szóhossz számításhoz
+    tisztitott_szavak = [szo for szo in szavak if any(c.isalpha() for c in szo)]
+
+    if not tisztitott_szavak:
+        return 0.0
+
+    osszes_hossz = sum(len(szo) for szo in tisztitott_szavak)
+    return osszes_hossz / len(tisztitott_szavak)
 
 
 def betu_statisztika_SZPL(szoveg):
     maganhangzok = "aáeéiíoóöőuúüűAÁEÉIÍOÓÖŐUÚÜŰ"
+    massalhangzok = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+
     magan_szam = 0
     massal_szam = 0
 
-    csak_betuk = "".join(filter(str.isalpha, szoveg))
-
-    for betu in csak_betuk:
-        if betu in maganhangzok:
+    for karakter in szoveg:
+        if karakter in maganhangzok:
             magan_szam += 1
-        else:
+        elif karakter in massalhangzok:
             massal_szam += 1
 
     return magan_szam, massal_szam
+
+
+def szamjegyek_szama_SZPL(szoveg):
+    szamjegy_szam = 0
+    for karakter in szoveg:
+        if karakter.isdigit():
+            szamjegy_szam += 1
+    return szamjegy_szam
